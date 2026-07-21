4a2b764792bb0d31c47aad95d771ad794443fdb7
Anas <anashamid794@gmail.com>
2026-07-21 16:28:05 +0500

    ttesting API endpoint

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
-    username=None
     email = models.EmailField(unique=True)
     email_verified = models.BooleanField(default=False)
     USERNAME_FIELD = "email" 
