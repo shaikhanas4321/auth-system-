ce955a9b3872059037eccbd8e20652d5fa74a938
Anas <anashamid794@gmail.com>
2026-07-21 17:06:21 +0500

    register API worked

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     email_verified = models.BooleanField(default=False)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
-        fields=["email","password",]
+        fields=["username","email","password"]
         extra_kwargs = {
           "password": {
         "write_only": True
