87b7e84890113abf2ee06c7fd8e8e0dc4945b339
Anas <anashamid794@gmail.com>
2026-07-21 16:36:54 +0500

    TEST

--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,3 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     email_verified = models.BooleanField(default=False)
-    USERNAME_FIELD = "email" 
-    REQUIRED_FIELDS = []
